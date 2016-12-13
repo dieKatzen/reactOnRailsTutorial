f19eec648de25bfff4b444a4d5b92ce44ce455a7
Tom <jjtomchen@gmail.com>
2016-12-12 22:06:22 -0500

    tutorial finished

---
+++ rubyNotes.rb
@@ -0,0 +1,23 @@
+#######################print ###############################
+puts new line
+
+print no new line
+
+
+#######################loops ###############################
+
+numbers = [1,2,3,4,5]
+
+for number in numbers
+
+  puts "#{number},"
+
+end
+
+letters = ["a","b","c","d"]
+
+letters.each do |food|  ###holding variable
+  puts get "Get some #{food}"
+
+
+  ####################### functions ###############################
